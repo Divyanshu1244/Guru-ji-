8379913b55e503361c2fabd4e4e00a2fad1f4d28
Divyanshu1244 <126379435+Divyanshu1244@users.noreply.github.com>
2025-12-28 15:01:59 +0530

    Update start_handler.py

--- bot/plugins/start_handler.py
+++ bot/plugins/start_handler.py
@@ -138,6 +138,6 @@ async def start_handler(client: Client, message: Message):
         )
 
         if Config.START_PIC:
-            await message.reply_photo(photo=Config.START_PIC, caption=caption, reply_markup=buttons, quote=True)
+            await message.reply_photo(photo=Config.START_PIC, caption=caption, reply_markup=buttons)
         else:
-            await message.reply_text(text=caption, reply_markup=buttons, disable_web_page_preview=True, quote=True)
+            await message.reply_text(text=caption, reply_markup=buttons, disable_web_page_preview=True)
