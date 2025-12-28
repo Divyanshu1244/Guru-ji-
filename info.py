2ea60e19190eaae39063c5a99508a721fe623d64
Divyanshu1244 <126379435+Divyanshu1244@users.noreply.github.com>
2025-12-28 13:45:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ class Config(object):
 
     AUTO_DELETE_TIME = int(getenv("AUTO_DELETE_TIME", "0"))
     AUTO_DELETE_MSG = getenv("AUTO_DELETE_MSG", "️ <b>Note:</b> Files will be automatically delete after <b>30 Minutes</b>.")
-    AUTO_DEL_SUCCESS_MSG = getenv("AUTO_DEL_SUCCESS_MSG", "Join our backup Channel/n lan channel ")
+    AUTO_DEL_SUCCESS_MSG = getenv("AUTO_DEL_SUCCESS_MSG", "✅ File deleted successfully.")
 
     # Token Verification (Shortlink)
     VERIFY_MODE = getenv("VERIFY_MODE", "True").lower() in ("true", "1", "yes")
